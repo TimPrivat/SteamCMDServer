23934a368d32c286044071a6d68cdc20153b8044
TimPrivat <timleon2701@gmail.com>
2024-05-18 12:59:28 +0200

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-ls
-echo "$(./SteamTOTPGenerator-linux $SteamAccountSecret)"
-./steamcmd.sh +runscript steamscript.txt
+ONE_TIME_CODE=$(./SteamTOTPGenerator-linux $SteamAccountSecret)
+export ONE_TIME_CODE=$ONE_TIME_CODE
+echo "One Time Code: $ONE_TIME_CODE"
+
+printenv | grep "ONE_TIME_CODE"
+
+
+#./steamcmd.sh +runscript steamscript.txt
 
