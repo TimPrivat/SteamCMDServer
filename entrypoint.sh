c7aefd4add2286315f4ab19960e10829d39797cf
TimPrivat <timleon2701@gmail.com>
2024-05-18 17:36:56 +0200

    delete steamscript

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-#ONE_TIME_CODE=$(./SteamTOTPGenerator-linux $SteamAccountSecret)
-#export ONE_TIME_CODE=$ONE_TIME_CODE
-
 main() {
 
-    APP_ID=60
+    APP_ID=	113200
     NAME=$(convertIDtoName $APP_ID)
+    ONE_TIME_CODE=$(./SteamTOTPGenerator-linux $SteamAccountSecret)
     echo "APPID $APP_ID: $NAME"
+
     DIR_PATH="/srv/samba/games/Steam/steamapps/common/$NAME"
     mkdir -p $DIR_PATH
     ./steamcmd.sh +force_install_dir $DIR_PATH +login $STEAM_USERNAME $STEAM_PASSWORD $ONE_TIME_CODE +@sSteamCmdForcePlatformType windows +app_update $APP_ID +quit
 
 }
 
-
-convertIDtoName (){
+convertIDtoName() {
     local APPID=$1
     local NAME=$(curl https://api.steampowered.com/ISteamApps/GetAppList/v0002/ | jq -r ".applist.apps | .[] | select(.appid==$APPID).name")
 
     echo $NAME
 }
 
-
-
 main
-
