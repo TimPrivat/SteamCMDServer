47d356b55b53b091e9f9b9b69c2454ff7b96c403
TimPrivat <timleon2701@gmail.com>
2024-05-18 15:42:15 +0200

    calls force install dir befoe logon

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ echo "One Time Code: $ONE_TIME_CODE"
 printenv | grep "ONE_TIME_CODE"
 printenv | grep "USER"
 
-./steamcmd.sh +login $STEAM_USERNAME $STEAM_PASSWORD $ONE_TIME_CODE +runscript steamscript.txt
+./steamcmd.sh +force_install_dir /srv/samba/games/Steam +login $STEAM_USERNAME $STEAM_PASSWORD $ONE_TIME_CODE +runscript steamscript.txt
 
