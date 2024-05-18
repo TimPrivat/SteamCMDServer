2c400aa883616bedf78f4d853617f694f0954713
TimPrivat <timleon2701@gmail.com>
2024-05-18 13:11:10 +0200

    add Steamlogin to Useraccount

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ echo "One Time Code: $ONE_TIME_CODE"
 printenv | grep "ONE_TIME_CODE"
 
 
-./steamcmd.sh +runscript steamscript.txt
+./steamcmd.sh +set_steam_guard_code $ONE_TIME_CODE +login $STEAM_USERNAME $STEAM_PASSWORD +runscript steamscript.txt
 
