98fcce2ea80e97d538865a8a589b0755f7cb9922
TimPrivat <timleon2701@gmail.com>
2024-05-19 15:48:39 +0200

    Add envfile and curl for all games

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,17 @@
 
 main() {
 
-    APP_ID=113200
-    NAME=$(convertIDtoName $APP_ID)
-    ONE_TIME_CODE=$(./SteamTOTPGenerator-linux $SteamAccountSecret)
-    echo "APPID $APP_ID: $NAME"
+    # APP_ID=113200
+    # NAME=$(convertIDtoName $APP_ID)
+    # ONE_TIME_CODE=$(./SteamTOTPGenerator-linux $SteamAccountSecret)
+    # echo "APPID $APP_ID: $NAME"
 
-    DIR_PATH="/srv/samba/games/Steam/steamapps/common/$NAME"
-    mkdir -p "$DIR_PATH"
-    ./steamcmd.sh +force_install_dir "$DIR_PATH" +login $STEAM_USERNAME $STEAM_PASSWORD $ONE_TIME_CODE +@sSteamCmdForcePlatformType windows +app_update $APP_ID +quit \
-     && mv "$DIR_PATH/steamapps/appmanifest_$APP_ID.acf" /srv/samba/games/Steam/steamapps/ \
-     && rm -rf /srv/samba/games/Steam/steamapps/common/$NAME/steamapps
+    # DIR_PATH="/srv/samba/games/Steam/steamapps/common/$NAME"
+    # mkdir -p "$DIR_PATH"
+    # ./steamcmd.sh +force_install_dir "$DIR_PATH" +login $STEAM_USERNAME $STEAM_PASSWORD $ONE_TIME_CODE +@sSteamCmdForcePlatformType windows +app_update $APP_ID +quit \
+    #  && mv "$DIR_PATH/steamapps/appmanifest_$APP_ID.acf" /srv/samba/games/Steam/steamapps/ \
+    #  && rm -rf /srv/samba/games/Steam/steamapps/common/$NAME/steamapps
+    receiveAllOwnedGames
 }
 
 receiveAllOwnedGames() {
